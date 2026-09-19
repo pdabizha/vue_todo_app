cfbdce3f71054c641e3c0972440822229c6f4604
Polina Dabizha <polinkadabi29@gmail.com>
2026-09-19 20:25:10 +0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue_todo_app/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
